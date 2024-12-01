d7eb3560944f912be63faefe93e34f930e05352a
zeyachen <zeya.chen@mail.utoronto.com>
2024-11-30 22:49:34 -0500

    more documentation

--- R/n_rec.R
+++ R/n_rec.R
@@ -10,14 +10,14 @@ n_rec <- function(
     n,
     ...
 ) {
-  if (ni - np <= dsearch) {
+  if (ni - np <= dsearch) { # return the increment of sample size from binary search
     return(c(np:ni))
-  } else if (p > pwr) {
+  } else if (p > pwr) { # set upper bound to current iterates and search smaller side
     ni <- n
     n <- ceiling((np + ni) / 2)
     p <- power_calc_fn(total_individuals = n, ...)$power
     n_rec(np = np, ni = ni, power_calc_fn = power_calc_fn, dsearch = dsearch, pwr = pwr, p = p, n = n, ...)
-  } else if (p < pwr) {
+  } else if (p < pwr) { # set lower bound to current iterates and search larger side
     np <- n
     n <- ceiling((np + ni) / 2)
     p <- power_calc_fn(total_individuals = n, ...)$power
